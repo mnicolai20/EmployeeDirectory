e16b5c8a5c602c8524216d0f6a26279d2a760fa6
nicolai <mattrnicolai@gmail.com>
2020-07-08 17:28:04 -0400

    edited app.js and api.js

---
+++ src/components/employeeInfo/EmployeeInfo.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import API from "../../utils/API";
+
+
+
+// Do we have to make our own employee info
+class EmployeeDetail extends Component {
+    render() { 
+        return(
+        <div className="text-center">
+            test
+        </div>
+    )
+    }
+};
+
+export default EmployeeDetail;
